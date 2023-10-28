6b92a2d61cce339071c3a48b36848f361f0f8e2f
Davide Ginelli <ginellidavide@gmail.com>
2023-10-28 12:44:07 +0200

    Create Constants.java

---
+++ app/src/main/java/it/unimib/worldnews/util/Constants.java
@@ -0,0 +1,35 @@
+package it.unimib.worldnews.util;
+
+/**
+ * Utility class to save constants used by the app.
+ */
+public class Constants {
+
+    // Constants for NewsAPI.org
+    public static final String FRANCE = "fr";
+    public static final String ITALY = "it";
+    public static final String GERMANY = "de";
+    public static final String UNITED_KINGDOM = "gb";
+    public static final String UNITED_STATES = "us";
+
+    public static final String BUSINESS = "business";
+    public static final String ENTERTAINMENT = "entertainment";
+    public static final String GENERAL = "general";
+    public static final String HEALTH = "health";
+    public static final String SCIENCE = "science";
+    public static final String SPORTS = "sports";
+    public static final String TECHNOLOGY = "technology";
+
+    // Constants for SharedPreferences
+    public static final String SHARED_PREFERENCES_FILE_NAME = "it.unimib.worldnews.preferences";
+    public static final String SHARED_PREFERENCES_COUNTRY_OF_INTEREST = "country_of_interest";
+    public static final String SHARED_PREFERENCES_TOPICS_OF_INTEREST = "topics_of_interest";
+
+    // Constants for EncryptedSharedPreferences
+    public static final String ENCRYPTED_SHARED_PREFERENCES_FILE_NAME = "it.unimib.worldnews.encrypted_preferences";
+    public static final String EMAIL_ADDRESS = "email_address";
+    public static final String PASSWORD = "password";
+
+    // Constants for encrypted files
+    public static final String ENCRYPTED_DATA_FILE_NAME = "it.unimib.worldnews.encrypted_file.txt";
+}
