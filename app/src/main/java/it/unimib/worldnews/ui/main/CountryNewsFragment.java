d0acd130a0abeb49682ae8e91f347cd89b037d04
Davide Ginelli <ginellidavide@gmail.com>
2023-11-01 21:57:16 +0100

    Create CountryNewsFragment

---
+++ app/src/main/java/it/unimib/worldnews/ui/main/CountryNewsFragment.java
@@ -0,0 +1,67 @@
+package it.unimib.worldnews.ui.main;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.view.MenuProvider;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import it.unimib.worldnews.R;
+
+/**
+ * Fragment that shows the news associated with a Country.
+ */
+public class CountryNewsFragment extends Fragment {
+
+    private static final String TAG = CountryNewsFragment.class.getSimpleName();
+
+    public CountryNewsFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment.
+     *
+     * @return A new instance of fragment CountryNewsFragment.
+     */
+    public static CountryNewsFragment newInstance() {
+        return new CountryNewsFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_country_news, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        requireActivity().addMenuProvider(new MenuProvider() {
+            @Override
+            public void onCreateMenu(@NonNull Menu menu, @NonNull MenuInflater menuInflater) {
+                menu.clear();
+            }
+
+            @Override
+            public boolean onMenuItemSelected(@NonNull MenuItem menuItem) {
+                return false;
+            }
+        });
+    }
+}
