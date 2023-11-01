b3ca9f04835b368f2204872400e9b940aedc3e12
Davide Ginelli <ginellidavide@gmail.com>
2023-11-01 21:55:00 +0100

    Add dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,13 +32,17 @@ android {
 }
 
 dependencies {
+    val nav_version = "2.7.5"
 
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.android.gms:play-services-base:18.2.0")
     implementation("commons-validator:commons-validator:1.7")
+    implementation("com.google.android.gms:play-services-location:21.0.1")
     implementation("androidx.security:security-crypto:1.1.0-alpha06")
+    implementation("androidx.navigation:navigation-fragment:$nav_version")
+    implementation("androidx.navigation:navigation-ui:$nav_version")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
