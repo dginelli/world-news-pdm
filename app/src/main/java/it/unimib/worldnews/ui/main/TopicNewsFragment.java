e3bee56d8ce94fe7451932190897e51be16a2b99
Davide Ginelli <ginellidavide@gmail.com>
2023-11-01 21:57:37 +0100

    Create TopicNewsFragment

---
+++ app/src/main/java/it/unimib/worldnews/ui/main/TopicNewsFragment.java
@@ -0,0 +1,65 @@
+package it.unimib.worldnews.ui.main;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.view.MenuProvider;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import it.unimib.worldnews.R;
+
+/**
+ * Fragment that shows the news associated with topics of interest of the user.
+ */
+public class TopicNewsFragment extends Fragment {
+
+    public TopicNewsFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment.
+     *
+     * @return A new instance of fragment TopicNewsFragment.
+     */
+    public static TopicNewsFragment newInstance() {
+        return new TopicNewsFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_topic_news, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        requireActivity().addMenuProvider(new MenuProvider() {
+            @Override
+            public void onCreateMenu(@NonNull Menu menu, @NonNull MenuInflater menuInflater) {
+                menu.clear();
+            }
+
+            @Override
+            public boolean onMenuItemSelected(@NonNull MenuItem menuItem) {
+                return false;
+            }
+        });
+    }
+}
