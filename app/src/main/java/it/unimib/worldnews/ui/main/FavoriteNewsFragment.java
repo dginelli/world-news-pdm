c4ad11b54272dff650d99713216e9c1cd14ccedd
Davide Ginelli <ginellidavide@gmail.com>
2023-11-08 22:35:48 +0100

    Update FavoriteNewsFragment

--- app/src/main/java/it/unimib/worldnews/ui/main/FavoriteNewsFragment.java
+++ app/src/main/java/it/unimib/worldnews/ui/main/FavoriteNewsFragment.java
@@ -1,5 +1,7 @@
 package it.unimib.worldnews.ui.main;
 
+import static it.unimib.worldnews.util.Constants.NEWS_API_TEST_JSON_FILE;
+
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -15,10 +17,22 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.google.android.material.snackbar.Snackbar;
 
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.util.List;
+
 import it.unimib.worldnews.R;
+import it.unimib.worldnews.adapter.NewsArrayAdapter;
+import it.unimib.worldnews.adapter.NewsBaseAdapter;
+import it.unimib.worldnews.adapter.NewsListAdapter;
+import it.unimib.worldnews.model.News;
+import it.unimib.worldnews.util.JSONParserUtil;
 
 /**
  * Fragment that shows the favorite news of the user.
@@ -26,6 +40,10 @@ import it.unimib.worldnews.R;
 public class FavoriteNewsFragment extends Fragment {
 
     private static final String TAG = FavoriteNewsFragment.class.getSimpleName();
+
+    private ListView listViewFavNews;
+    private News[] newsArray;
+    private List<News> newsList;
 
     public FavoriteNewsFragment() {
         // Required empty public constructor
@@ -44,6 +62,16 @@ public class FavoriteNewsFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        JSONParserUtil jsonParserUtil = new JSONParserUtil(requireActivity().getApplication());
+        try {
+            newsList =
+                    jsonParserUtil.parseJSONFileWithJSONObjectArray(NEWS_API_TEST_JSON_FILE).
+                            getArticles().subList(0, 10);
+            newsArray = newsList.toArray(new News[0]);
+        } catch (IOException | JSONException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -67,13 +95,69 @@ public class FavoriteNewsFragment extends Fragment {
             @Override
             public boolean onMenuItemSelected(@NonNull MenuItem menuItem) {
                 if (menuItem.getItemId() == R.id.delete) {
-                    Snackbar.make(requireView(), "Delete menu item pressed",
-                                    Snackbar.LENGTH_SHORT).show();
+                    Log.d(TAG, "Delete menu item pressed");
                 }
                 return false;
             }
             // Use getViewLifecycleOwner() to avoid that the listener
             // associated with a menu icon is called twice
         }, getViewLifecycleOwner(), Lifecycle.State.RESUMED);
+
+        listViewFavNews = view.findViewById(R.id.listview_fav_news);
+
+        // Use one of these four methods to populate the ListView:
+        // 1) useDefaultLisAdapter(); 2) useCustomArrayAdapter();
+        // 3) useCustomBaseAdapter(); 4) useCustomListAdapter();
+        useCustomListAdapter();
+
+        listViewFavNews.setOnItemClickListener((parent, view1, position, id) ->
+                Snackbar.make(requireActivity().findViewById(android.R.id.content),
+                        newsList.get(position).getTitle(), Snackbar.LENGTH_SHORT).show());
+    }
+
+    /**
+     * Creates an ArrayAdapter to be used to populate the ListView
+     * R.id.listview_fav_news (listViewFavNews) with an array of News.
+     */
+    private void useDefaultLisAdapter() {
+        ArrayAdapter<News> adapter = new ArrayAdapter<News>(requireContext(),
+                android.R.layout.simple_list_item_1, newsArray);
+        listViewFavNews.setAdapter(adapter);
+    }
+
+    /**
+     * Creates a custom ArrayAdapter to be used to populate the ListView
+     * R.id.listview_fav_news (listViewFavNews) with an array of News.
+     */
+    private void useCustomArrayAdapter() {
+        NewsArrayAdapter newsListArrayAdapter =
+                new NewsArrayAdapter(requireContext(), R.layout.fav_news_list_item, newsArray);
+        listViewFavNews.setAdapter(newsListArrayAdapter);
+    }
+
+    /**
+     * Creates a custom ArrayAdapter to be used to populate the ListView
+     * R.id.listview_fav_news (listViewFavNews) with an ArrayList of News.
+     */
+    private void useCustomListAdapter() {
+        NewsListAdapter newsListAdapter =
+                new NewsListAdapter(requireContext(), R.layout.fav_news_list_item, newsList,
+                        new NewsListAdapter.OnDeleteButtonClickListener() {
+                            @Override
+                            public void onDeleteButtonClick(News news) {
+                                Snackbar.make(listViewFavNews,
+                                        news.getTitle(), Snackbar.LENGTH_SHORT).show();
+                            }
+                        });
+        listViewFavNews.setAdapter(newsListAdapter);
+    }
+
+    /**
+     * Creates a custom BaseAdapter to be used to populate the ListView
+     * R.id.listview_fav_news (listViewFavNews).
+     */
+    private void useCustomBaseAdapter() {
+        NewsBaseAdapter newsListBaseAdapter = new NewsBaseAdapter(newsList);
+        listViewFavNews.setAdapter(newsListBaseAdapter);
     }
 }
