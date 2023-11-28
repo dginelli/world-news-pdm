b38d37199f28b1a5372fc4ffbe6bb3bb7c5474c3
Davide Ginelli <ginellidavide@gmail.com>
2023-11-28 08:05:45 +0100

    Create NewsApiService.java

---
+++ app/src/main/java/it/unimib/worldnews/service/NewsApiService.java
@@ -0,0 +1,22 @@
+package it.unimib.worldnews.service;
+
+import static it.unimib.worldnews.util.Constants.TOP_HEADLINES_COUNTRY_PARAMETER;
+import static it.unimib.worldnews.util.Constants.TOP_HEADLINES_ENDPOINT;
+import static it.unimib.worldnews.util.Constants.TOP_HEADLINES_PAGE_SIZE_PARAMETER;
+
+import it.unimib.worldnews.model.NewsApiResponse;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.Query;
+
+/**
+ * Interface for Service to get news from the Web Service.
+ */
+public interface NewsApiService {
+    @GET(TOP_HEADLINES_ENDPOINT)
+    Call<NewsApiResponse> getNews(
+            @Query(TOP_HEADLINES_COUNTRY_PARAMETER) String country,
+            @Query(TOP_HEADLINES_PAGE_SIZE_PARAMETER) int pageSize,
+            @Header("Authorization") String apiKey);
+}
