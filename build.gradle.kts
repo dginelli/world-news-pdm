e6b999d53d96643c5d02d6bd996db2a125ddb6d3
Davide Ginelli <ginellidavide@gmail.com>
2023-11-28 08:03:14 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,17 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+    }
+    dependencies {
+        val navVersion = "2.7.5"
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$navVersion")
+    }
+}
+
+
 plugins {
     id("com.android.application") version "8.1.2" apply false
 }
