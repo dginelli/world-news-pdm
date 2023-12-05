1ec948a6293cf438f1ec0acd12a393106ee759c6
Davide Ginelli <ginellidavide@gmail.com>
2023-12-05 20:34:30 +0100

    Update ServiceLocator.java

--- app/src/main/java/it/unimib/worldnews/util/ServiceLocator.java
+++ app/src/main/java/it/unimib/worldnews/util/ServiceLocator.java
@@ -2,8 +2,16 @@ package it.unimib.worldnews.util;
 
 import android.app.Application;
 
+import it.unimib.worldnews.R;
 import it.unimib.worldnews.database.NewsRoomDatabase;
+import it.unimib.worldnews.repository.INewsRepositoryWithLiveData;
+import it.unimib.worldnews.repository.NewsRepositoryWithLiveData;
 import it.unimib.worldnews.service.NewsApiService;
+import it.unimib.worldnews.source.BaseNewsLocalDataSource;
+import it.unimib.worldnews.source.BaseNewsRemoteDataSource;
+import it.unimib.worldnews.source.NewsLocalDataSource;
+import it.unimib.worldnews.source.NewsMockRemoteDataSource;
+import it.unimib.worldnews.source.NewsRemoteDataSource;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -17,6 +25,10 @@ public class ServiceLocator {
 
     private ServiceLocator() {}
 
+    /**
+     * Returns an instance of ServiceLocator class.
+     * @return An instance of ServiceLocator.
+     */
     public static ServiceLocator getInstance() {
         if (INSTANCE == null) {
             synchronized(ServiceLocator.class) {
@@ -29,7 +41,7 @@ public class ServiceLocator {
     }
 
     /**
-     * It creates an instance of NewsApiService using Retrofit.
+     * Returns an instance of NewsApiService class using Retrofit.
      * @return an instance of NewsApiService.
      */
     public NewsApiService getNewsApiService() {
@@ -38,7 +50,37 @@ public class ServiceLocator {
         return retrofit.create(NewsApiService.class);
     }
 
+    /**
+     * Returns an instance of NewsRoomDatabase class to manage Room database.
+     * @param application Param for accessing the global application state.
+     * @return An instance of NewsRoomDatabase.
+     */
     public NewsRoomDatabase getNewsDao(Application application) {
         return NewsRoomDatabase.getDatabase(application);
+    }
+
+    /**
+     * Returns an instance of INewsRepositoryWithLiveData.
+     * @param application Param for accessing the global application state.
+     * @param debugMode Param to establish if the application is run in debug mode.
+     * @return An instance of INewsRepositoryWithLiveData.
+     */
+    public INewsRepositoryWithLiveData getNewsRepository(Application application, boolean debugMode) {
+        BaseNewsRemoteDataSource newsRemoteDataSource;
+        BaseNewsLocalDataSource newsLocalDataSource;
+        SharedPreferencesUtil sharedPreferencesUtil = new SharedPreferencesUtil(application);
+
+        if (debugMode) {
+            JSONParserUtil jsonParserUtil = new JSONParserUtil(application);
+            newsRemoteDataSource =
+                    new NewsMockRemoteDataSource(jsonParserUtil, JSONParserUtil.JsonParserType.GSON);
+        } else {
+            newsRemoteDataSource =
+                    new NewsRemoteDataSource(application.getString(R.string.news_api_key));
+        }
+
+        newsLocalDataSource = new NewsLocalDataSource(getNewsDao(application), sharedPreferencesUtil);
+
+        return new NewsRepositoryWithLiveData(newsRemoteDataSource, newsLocalDataSource);
     }
 }
