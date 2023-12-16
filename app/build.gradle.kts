336406853017bff356827a60521f24eaf83fd5da
Davide Ginelli <ginellidavide@gmail.com>
2023-12-16 11:47:10 +0100

    Update app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "it.unimib.worldnews"
         minSdk = 23
         targetSdk = 34
-        versionCode = 6
-        versionName = "0.6"
+        versionCode = 7
+        versionName = "0.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
