f8712928e2f8e02b22ea7843c9187cb7a6ebe878
Davide Ginelli <ginellidavide@gmail.com>
2024-01-02 15:03:07 +0100

    Update WelcomeActivity.java

--- app/src/main/java/it/unimib/worldnews/ui/welcome/WelcomeActivity.java
+++ app/src/main/java/it/unimib/worldnews/ui/welcome/WelcomeActivity.java
@@ -1,6 +1,10 @@
 package it.unimib.worldnews.ui.welcome;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
@@ -17,5 +21,15 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        Toolbar toolbar = findViewById(R.id.top_appbar);
+        setSupportActionBar(toolbar);
+
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().
+                findFragmentById(R.id.nav_host_fragment);
+        NavController navController = navHostFragment.getNavController();
+
+        // For the Toolbar
+        NavigationUI.setupActionBarWithNavController(this, navController);
     }
 }
