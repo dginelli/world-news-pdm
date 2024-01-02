66511e499a5df5a9016290e78429510e63245f1a
Davide Ginelli <ginellidavide@gmail.com>
2024-01-02 14:59:58 +0100

    Add logic to manage the user authentication and data

---
+++ app/src/main/java/it/unimib/worldnews/ui/welcome/UserViewModelFactory.java
@@ -0,0 +1,27 @@
+package it.unimib.worldnews.ui.welcome;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+
+import it.unimib.worldnews.repository.user.IUserRepository;
+
+
+/**
+ * Custom ViewModelProvider to be able to have a custom constructor
+ * for the UserViewModel class.
+ */
+public class UserViewModelFactory implements ViewModelProvider.Factory {
+
+    private final IUserRepository userRepository;
+
+    public UserViewModelFactory(IUserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @NonNull
+    @Override
+    public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+        return (T) new UserViewModel(userRepository);
+    }
+}
