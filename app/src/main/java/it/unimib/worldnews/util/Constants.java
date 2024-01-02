279245c1feffbe3a9b4a799731b730d963cb6899
Davide Ginelli <ginellidavide@gmail.com>
2024-01-02 15:00:12 +0100

    Update Constants.java

--- app/src/main/java/it/unimib/worldnews/util/Constants.java
+++ app/src/main/java/it/unimib/worldnews/util/Constants.java
@@ -24,11 +24,13 @@ public class Constants {
     public static final String SHARED_PREFERENCES_FILE_NAME = "it.unimib.worldnews.preferences";
     public static final String SHARED_PREFERENCES_COUNTRY_OF_INTEREST = "country_of_interest";
     public static final String SHARED_PREFERENCES_TOPICS_OF_INTEREST = "topics_of_interest";
+    public static final String SHARED_PREFERENCES_FIRST_LOADING = "first_loading";
 
     // Constants for EncryptedSharedPreferences
     public static final String ENCRYPTED_SHARED_PREFERENCES_FILE_NAME = "it.unimib.worldnews.encrypted_preferences";
     public static final String EMAIL_ADDRESS = "email_address";
     public static final String PASSWORD = "password";
+    public static final String ID_TOKEN = "google_token";
 
     // Constants for encrypted files
     public static final String ENCRYPTED_DATA_FILE_NAME = "it.unimib.worldnews.encrypted_file.txt";
@@ -55,4 +57,15 @@ public class Constants {
     public static final String RETROFIT_ERROR = "retrofit_error";
     public static final String API_KEY_ERROR = "api_key_error";
     public static final String UNEXPECTED_ERROR = "unexpected_error";
+    public static final String INVALID_USER_ERROR = "invalidUserError";
+    public static final String INVALID_CREDENTIALS_ERROR = "invalidCredentials";
+    public static final String USER_COLLISION_ERROR = "userCollisionError";
+    public static final String WEAK_PASSWORD_ERROR = "passwordIsWeak";
+
+    public static final int MINIMUM_PASSWORD_LENGTH = 6;
+
+    // Constants for Firebase Realtime Database
+    public static final String FIREBASE_REALTIME_DATABASE = "https://world-news-pdm-default-rtdb.europe-west1.firebasedatabase.app/";
+    public static final String FIREBASE_USERS_COLLECTION = "users";
+    public static final String FIREBASE_FAVORITE_NEWS_COLLECTION = "favorite_news";
 }
