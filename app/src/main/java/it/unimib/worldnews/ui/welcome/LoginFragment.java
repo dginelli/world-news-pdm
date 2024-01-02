cb18acd955c955cf1152dc987f49ff8ea206482e
Davide Ginelli <ginellidavide@gmail.com>
2024-01-02 15:03:09 +0100

    Update LoginFragment.java

--- app/src/main/java/it/unimib/worldnews/ui/welcome/LoginFragment.java
+++ app/src/main/java/it/unimib/worldnews/ui/welcome/LoginFragment.java
@@ -3,22 +3,25 @@ package it.unimib.worldnews.ui.welcome;
 import static it.unimib.worldnews.util.Constants.EMAIL_ADDRESS;
 import static it.unimib.worldnews.util.Constants.ENCRYPTED_DATA_FILE_NAME;
 import static it.unimib.worldnews.util.Constants.ENCRYPTED_SHARED_PREFERENCES_FILE_NAME;
+import static it.unimib.worldnews.util.Constants.INVALID_CREDENTIALS_ERROR;
+import static it.unimib.worldnews.util.Constants.INVALID_USER_ERROR;
 import static it.unimib.worldnews.util.Constants.PASSWORD;
 import static it.unimib.worldnews.util.Constants.SHARED_PREFERENCES_COUNTRY_OF_INTEREST;
 import static it.unimib.worldnews.util.Constants.SHARED_PREFERENCES_FILE_NAME;
+import static it.unimib.worldnews.util.Constants.ID_TOKEN;
 import static it.unimib.worldnews.util.Constants.SHARED_PREFERENCES_TOPICS_OF_INTEREST;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 
-import androidx.activity.result.ActivityResult;
-import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.IntentSenderRequest;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
 
 import android.util.Log;
@@ -27,6 +30,15 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import com.google.android.gms.auth.api.identity.BeginSignInRequest;
+import com.google.android.gms.auth.api.identity.BeginSignInResult;
+import com.google.android.gms.auth.api.identity.Identity;
+import com.google.android.gms.auth.api.identity.SignInClient;
+import com.google.android.gms.auth.api.identity.SignInCredential;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.progressindicator.LinearProgressIndicator;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputLayout;
 
@@ -36,40 +48,36 @@ import java.io.IOException;
 import java.security.GeneralSecurityException;
 
 import it.unimib.worldnews.R;
+import it.unimib.worldnews.model.Result;
+import it.unimib.worldnews.model.User;
+
+import it.unimib.worldnews.repository.user.IUserRepository;
 import it.unimib.worldnews.ui.main.MainActivityWithBottomNavigationView;
-import it.unimib.worldnews.ui.main.MainActivityWithNavigationDrawer;
 import it.unimib.worldnews.ui.preferences.NewsPreferencesActivity;
 import it.unimib.worldnews.util.DataEncryptionUtil;
+import it.unimib.worldnews.util.ServiceLocator;
 import it.unimib.worldnews.util.SharedPreferencesUtil;
 
 /**
- * Fragment that allows user to login.
+ * Fragment that allows the user to login.
  */
 public class LoginFragment extends Fragment {
 
     private static final String TAG = LoginFragment.class.getSimpleName();
     private static final boolean USE_NAVIGATION_COMPONENT = true;
 
+    private ActivityResultLauncher<IntentSenderRequest> activityResultLauncher;
+    private ActivityResultContracts.StartIntentSenderForResult startIntentSenderForResult;
+
+    private UserViewModel userViewModel;
     private TextInputLayout textInputLayoutEmail;
     private TextInputLayout textInputLayoutPassword;
+    private LinearProgressIndicator progressIndicator;
+
+    private SignInClient oneTapClient;
+    private BeginSignInRequest signInRequest;
 
     private DataEncryptionUtil dataEncryptionUtil;
-
-    ActivityResultLauncher<Intent> mStartForResult = registerForActivityResult(
-        new ActivityResultContracts.StartActivityForResult(),
-        new ActivityResultCallback<ActivityResult>() {
-            @Override
-            public void onActivityResult(ActivityResult result) {
-                if (result.getResultCode() == Activity.RESULT_OK) {
-                    Intent intent = result.getData();
-                    if (intent != null) {
-                        String resultFromActivity = intent.getStringExtra("ACTIVITY_FOR_RESULT");
-                        Log.d(TAG, "Result: " + resultFromActivity);
-                    }
-                }
-            }
-        }
-    );
 
     public LoginFragment() {
         // Required empty public constructor
@@ -88,6 +96,73 @@ public class LoginFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        IUserRepository userRepository = ServiceLocator.getInstance().
+                getUserRepository(requireActivity().getApplication());
+        userViewModel = new ViewModelProvider(
+                requireActivity(),
+                new UserViewModelFactory(userRepository)).get(UserViewModel.class);
+        dataEncryptionUtil = new DataEncryptionUtil(requireActivity().getApplication());
+
+        oneTapClient = Identity.getSignInClient(requireActivity());
+        signInRequest = BeginSignInRequest.builder()
+            .setPasswordRequestOptions(BeginSignInRequest.PasswordRequestOptions.builder()
+                    .setSupported(true)
+                    .build())
+            .setGoogleIdTokenRequestOptions(BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                    .setSupported(true)
+                    // Your server's client ID, not your Android client ID.
+                    .setServerClientId(getString(R.string.default_web_client_id))
+                    // Only show accounts previously used to sign in.
+                    .setFilterByAuthorizedAccounts(false)
+                    .build())
+            // Automatically sign in when exactly one credential is retrieved.
+            .setAutoSelectEnabled(true)
+            .build();
+
+        startIntentSenderForResult = new ActivityResultContracts.StartIntentSenderForResult();
+
+        activityResultLauncher = registerForActivityResult(startIntentSenderForResult, activityResult -> {
+            if (activityResult.getResultCode() == Activity.RESULT_OK) {
+                Log.d(TAG, "result.getResultCode() == Activity.RESULT_OK");
+                try {
+                    SignInCredential credential = oneTapClient.getSignInCredentialFromIntent(activityResult.getData());
+                    String idToken = credential.getGoogleIdToken();
+                    if (idToken !=  null) {
+                        // Got an ID token from Google. Use it to authenticate with Firebase.
+                        userViewModel.getGoogleUserMutableLiveData(idToken).observe(getViewLifecycleOwner(), authenticationResult -> {
+                            if (authenticationResult.isSuccess()) {
+                                User user = ((Result.UserResponseSuccess) authenticationResult).getData();
+                                saveLoginData(user.getEmail(), null, user.getIdToken());
+                                userViewModel.setAuthenticationError(false);
+                                retrieveUserInformationAndStartActivity(user, R.id.navigate_to_newsPreferencesActivity);
+                            } else {
+                                userViewModel.setAuthenticationError(true);
+                                progressIndicator.setVisibility(View.GONE);
+                                Snackbar.make(requireActivity().findViewById(android.R.id.content),
+                                        getErrorMessage(((Result.Error) authenticationResult).getMessage()),
+                                        Snackbar.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                } catch (ApiException e) {
+                        Snackbar.make(requireActivity().findViewById(android.R.id.content),
+                                requireActivity().getString(R.string.unexpected_error),
+                                Snackbar.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void retrieveUserInformationAndStartActivity(User user, int destination) {
+        progressIndicator.setVisibility(View.VISIBLE);
+
+        userViewModel.getUserFavoriteNewsMutableLiveData(user.getIdToken()).observe(
+            getViewLifecycleOwner(), userFavoriteNewsRetrievalResult -> {
+                progressIndicator.setVisibility(View.GONE);
+                startActivityBasedOnCondition(NewsPreferencesActivity.class, destination);
+            }
+        );
     }
 
     @Override
@@ -101,32 +176,30 @@ public class LoginFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        dataEncryptionUtil = new DataEncryptionUtil(requireActivity().getApplication());
-        try {
-            if (dataEncryptionUtil.readSecretDataOnFile(ENCRYPTED_DATA_FILE_NAME) != null) {
-                SharedPreferencesUtil sharedPreferencesUtil =
-                        new SharedPreferencesUtil(requireActivity().getApplication());
+        if (userViewModel.getLoggedUser() != null) {
+            SharedPreferencesUtil sharedPreferencesUtil =
+                    new SharedPreferencesUtil(requireActivity().getApplication());
 
-                if (sharedPreferencesUtil.readStringData(SHARED_PREFERENCES_FILE_NAME,
-                        SHARED_PREFERENCES_COUNTRY_OF_INTEREST) != null &&
-                        sharedPreferencesUtil.readStringSetData(SHARED_PREFERENCES_FILE_NAME,
-                                SHARED_PREFERENCES_TOPICS_OF_INTEREST) != null) {
+            if (sharedPreferencesUtil.readStringData(SHARED_PREFERENCES_FILE_NAME,
+                    SHARED_PREFERENCES_COUNTRY_OF_INTEREST) != null &&
+                    sharedPreferencesUtil.readStringSetData(SHARED_PREFERENCES_FILE_NAME,
+                            SHARED_PREFERENCES_TOPICS_OF_INTEREST) != null) {
 
-                    startActivityBasedOnCondition(MainActivityWithBottomNavigationView.class,
-                            R.id.navigate_to_mainActivityWithBottomNavigationView);
-                } else {
-                    startActivityBasedOnCondition(NewsPreferencesActivity.class,
-                            R.id.navigate_to_newsPreferencesActivity);
-                }
+                startActivityBasedOnCondition(MainActivityWithBottomNavigationView.class,
+                        R.id.navigate_to_mainActivityWithBottomNavigationView);
+            } else {
+                startActivityBasedOnCondition(NewsPreferencesActivity.class,
+                        R.id.navigate_to_newsPreferencesActivity);
             }
-        } catch (GeneralSecurityException | IOException e) {
-            e.printStackTrace();
         }
 
         textInputLayoutEmail = view.findViewById(R.id.text_input_layout_email);
         textInputLayoutPassword = view.findViewById(R.id.text_input_layout_password);
+        progressIndicator = view.findViewById(R.id.progress_bar);
+
         final Button buttonLogin = view.findViewById(R.id.button_login);
         final Button buttonGoogleLogin = view.findViewById(R.id.button_google_login);
+        final Button buttonRegistration = view.findViewById(R.id.button_registration);
 
         dataEncryptionUtil = new DataEncryptionUtil(requireActivity().getApplication());
 
@@ -137,6 +210,9 @@ public class LoginFragment extends Fragment {
             Log.d(TAG, "Password from encrypted SharedPref: " + dataEncryptionUtil.
                     readSecretDataWithEncryptedSharedPreferences(
                             ENCRYPTED_SHARED_PREFERENCES_FILE_NAME, PASSWORD));
+            Log.d(TAG, "Token from encrypted SharedPref: " + dataEncryptionUtil.
+                    readSecretDataWithEncryptedSharedPreferences(
+                            ENCRYPTED_SHARED_PREFERENCES_FILE_NAME, ID_TOKEN));
             Log.d(TAG, "Login data from encrypted file: " + dataEncryptionUtil.
                     readSecretDataOnFile(ENCRYPTED_DATA_FILE_NAME));
         } catch (GeneralSecurityException | IOException e) {
@@ -144,31 +220,85 @@ public class LoginFragment extends Fragment {
         }
 
         buttonLogin.setOnClickListener(v -> {
-
-            String email = textInputLayoutEmail.getEditText().getText().toString();
-            String password = textInputLayoutPassword.getEditText().getText().toString();
+            String email = textInputLayoutEmail.getEditText().getText().toString().trim();
+            String password = textInputLayoutPassword.getEditText().getText().toString().trim();
 
             // Start login if email and password are ok
             if (isEmailOk(email) & isPasswordOk(password)) {
-                Log.d(TAG, "Email and password are ok");
-                saveLoginData(email, password);
-
-                startActivityBasedOnCondition(NewsPreferencesActivity.class,
-                        R.id.navigate_to_newsPreferencesActivity);
+                if (!userViewModel.isAuthenticationError()) {
+                    progressIndicator.setVisibility(View.VISIBLE);
+                    userViewModel.getUserMutableLiveData(email, password, true).observe(
+                        getViewLifecycleOwner(), result -> {
+                            if (result.isSuccess()) {
+                                User user = ((Result.UserResponseSuccess) result).getData();
+                                saveLoginData(email, password, user.getIdToken());
+                                userViewModel.setAuthenticationError(false);
+                                retrieveUserInformationAndStartActivity(user, R.id.navigate_to_newsPreferencesActivity);
+                            } else {
+                                userViewModel.setAuthenticationError(true);
+                                progressIndicator.setVisibility(View.GONE);
+                                Snackbar.make(requireActivity().findViewById(android.R.id.content),
+                                        getErrorMessage(((Result.Error) result).getMessage()),
+                                        Snackbar.LENGTH_SHORT).show();
+                            }
+                        });
+                } else {
+                    userViewModel.getUser(email, password, true);
+                }
             } else {
                 Snackbar.make(requireActivity().findViewById(android.R.id.content),
                         R.string.check_login_data_message, Snackbar.LENGTH_SHORT).show();
             }
         });
 
-        buttonGoogleLogin.setOnClickListener(v ->
-                startActivityBasedOnCondition(MainActivityWithNavigationDrawer.class,
-                        R.id.navigate_to_mainActivityWithNavigationDrawer));
+        buttonGoogleLogin.setOnClickListener(v -> oneTapClient.beginSignIn(signInRequest)
+            .addOnSuccessListener(requireActivity(), new OnSuccessListener<BeginSignInResult>() {
+                @Override
+                public void onSuccess(BeginSignInResult result) {
+                    Log.d(TAG, "onSuccess from oneTapClient.beginSignIn(BeginSignInRequest)");
+                    IntentSenderRequest intentSenderRequest =
+                            new IntentSenderRequest.Builder(result.getPendingIntent()).build();
+                    activityResultLauncher.launch(intentSenderRequest);
+                }
+            })
+            .addOnFailureListener(requireActivity(), new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    // No saved credentials found. Launch the One Tap sign-up flow, or
+                    // do nothing and continue presenting the signed-out UI.
+                    Log.d(TAG, e.getLocalizedMessage());
 
-        final Button forgotPasswordButton = view.findViewById(R.id.button_forgot_password);
-        forgotPasswordButton.setOnClickListener(button -> {
-            mStartForResult.launch(new Intent(requireActivity(), NewsPreferencesActivity.class));
+                    Snackbar.make(requireActivity().findViewById(android.R.id.content),
+                            requireActivity().getString(R.string.error_no_google_account_found_message),
+                            Snackbar.LENGTH_SHORT).show();
+                }
+            }));
+
+        buttonRegistration.setOnClickListener(v -> {
+            Navigation.findNavController(view).navigate(R.id.navigate_to_registrationFragment);
         });
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        userViewModel.setAuthenticationError(false);
+    }
+
+    /**
+     * Returns the text to be shown to the user based on the type of error.
+     * @param errorType The type of error.
+     * @return The message to be shown to the user.
+     */
+    private String getErrorMessage(String errorType) {
+        switch (errorType) {
+            case INVALID_CREDENTIALS_ERROR:
+                return requireActivity().getString(R.string.error_login_password_message);
+            case INVALID_USER_ERROR:
+                return requireActivity().getString(R.string.error_login_user_message);
+            default:
+                return requireActivity().getString(R.string.unexpected_error);
+        }
     }
 
     /**
@@ -223,16 +353,22 @@ public class LoginFragment extends Fragment {
      * Encrypts login data using DataEncryptionUtil class.
      * @param email The email address to be encrypted and saved
      * @param password The password to be encrypted and saved
+     * @param idToken The token associated with the account
      */
-    private void saveLoginData(String email, String password) {
+    private void saveLoginData(String email, String password, String idToken) {
         try {
             dataEncryptionUtil.writeSecretDataWithEncryptedSharedPreferences(
                     ENCRYPTED_SHARED_PREFERENCES_FILE_NAME, EMAIL_ADDRESS, email);
             dataEncryptionUtil.writeSecretDataWithEncryptedSharedPreferences(
                     ENCRYPTED_SHARED_PREFERENCES_FILE_NAME, PASSWORD, password);
+            dataEncryptionUtil.writeSecretDataWithEncryptedSharedPreferences(
+                    ENCRYPTED_SHARED_PREFERENCES_FILE_NAME, ID_TOKEN, idToken);
 
-            dataEncryptionUtil.writeSecreteDataOnFile(ENCRYPTED_DATA_FILE_NAME,
-                    email.concat(":").concat(password));
+            if (password != null) {
+                dataEncryptionUtil.writeSecreteDataOnFile(ENCRYPTED_DATA_FILE_NAME,
+                        email.concat(":").concat(password));
+            }
+
         } catch (GeneralSecurityException | IOException e) {
             e.printStackTrace();
         }
