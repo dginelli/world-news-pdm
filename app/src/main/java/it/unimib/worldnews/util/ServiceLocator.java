53805f6cd9cfffc4bbb15f86e12819a9113d7a2f
Davide Ginelli <ginellidavide@gmail.com>
2024-01-02 15:03:13 +0100

    Update ServiceLocator.java

--- app/src/main/java/it/unimib/worldnews/util/ServiceLocator.java
+++ app/src/main/java/it/unimib/worldnews/util/ServiceLocator.java
@@ -1,17 +1,31 @@
 package it.unimib.worldnews.util;
 
+import static it.unimib.worldnews.util.Constants.ENCRYPTED_SHARED_PREFERENCES_FILE_NAME;
+import static it.unimib.worldnews.util.Constants.ID_TOKEN;
+
 import android.app.Application;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
 
 import it.unimib.worldnews.R;
 import it.unimib.worldnews.database.NewsRoomDatabase;
-import it.unimib.worldnews.repository.INewsRepositoryWithLiveData;
-import it.unimib.worldnews.repository.NewsRepositoryWithLiveData;
+import it.unimib.worldnews.repository.news.INewsRepositoryWithLiveData;
+import it.unimib.worldnews.repository.news.NewsRepositoryWithLiveData;
+import it.unimib.worldnews.repository.user.IUserRepository;
+import it.unimib.worldnews.repository.user.UserRepository;
 import it.unimib.worldnews.service.NewsApiService;
-import it.unimib.worldnews.source.BaseNewsLocalDataSource;
-import it.unimib.worldnews.source.BaseNewsRemoteDataSource;
-import it.unimib.worldnews.source.NewsLocalDataSource;
-import it.unimib.worldnews.source.NewsMockRemoteDataSource;
-import it.unimib.worldnews.source.NewsRemoteDataSource;
+import it.unimib.worldnews.source.news.BaseFavoriteNewsDataSource;
+import it.unimib.worldnews.source.news.BaseNewsLocalDataSource;
+import it.unimib.worldnews.source.news.BaseNewsRemoteDataSource;
+import it.unimib.worldnews.source.news.FavoriteNewsDataSource;
+import it.unimib.worldnews.source.news.NewsLocalDataSource;
+import it.unimib.worldnews.source.news.NewsMockRemoteDataSource;
+import it.unimib.worldnews.source.news.NewsRemoteDataSource;
+import it.unimib.worldnews.source.user.BaseUserAuthenticationRemoteDataSource;
+import it.unimib.worldnews.source.user.BaseUserDataRemoteDataSource;
+import it.unimib.worldnews.source.user.UserAuthenticationRemoteDataSource;
+import it.unimib.worldnews.source.user.UserDataRemoteDataSource;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -68,7 +82,9 @@ public class ServiceLocator {
     public INewsRepositoryWithLiveData getNewsRepository(Application application, boolean debugMode) {
         BaseNewsRemoteDataSource newsRemoteDataSource;
         BaseNewsLocalDataSource newsLocalDataSource;
+        BaseFavoriteNewsDataSource favoriteNewsDataSource;
         SharedPreferencesUtil sharedPreferencesUtil = new SharedPreferencesUtil(application);
+        DataEncryptionUtil dataEncryptionUtil = new DataEncryptionUtil(application);
 
         if (debugMode) {
             JSONParserUtil jsonParserUtil = new JSONParserUtil(application);
@@ -79,8 +95,43 @@ public class ServiceLocator {
                     new NewsRemoteDataSource(application.getString(R.string.news_api_key));
         }
 
-        newsLocalDataSource = new NewsLocalDataSource(getNewsDao(application), sharedPreferencesUtil);
+        newsLocalDataSource = new NewsLocalDataSource(getNewsDao(application),
+                sharedPreferencesUtil, dataEncryptionUtil);
 
-        return new NewsRepositoryWithLiveData(newsRemoteDataSource, newsLocalDataSource);
+        try {
+            favoriteNewsDataSource = new FavoriteNewsDataSource(dataEncryptionUtil.
+                    readSecretDataWithEncryptedSharedPreferences(
+                            ENCRYPTED_SHARED_PREFERENCES_FILE_NAME, ID_TOKEN
+                    )
+            );
+        } catch (GeneralSecurityException | IOException e) {
+            return null;
+        }
+
+        return new NewsRepositoryWithLiveData(newsRemoteDataSource,
+                newsLocalDataSource, favoriteNewsDataSource);
+    }
+
+    /**
+     * Creates an instance of IUserRepository.
+     * @return An instance of IUserRepository.
+     */
+    public IUserRepository getUserRepository(Application application) {
+        SharedPreferencesUtil sharedPreferencesUtil = new SharedPreferencesUtil(application);
+
+        BaseUserAuthenticationRemoteDataSource userRemoteAuthenticationDataSource =
+                new UserAuthenticationRemoteDataSource();
+
+        BaseUserDataRemoteDataSource userDataRemoteDataSource =
+                new UserDataRemoteDataSource(sharedPreferencesUtil);
+
+        DataEncryptionUtil dataEncryptionUtil = new DataEncryptionUtil(application);
+
+        BaseNewsLocalDataSource newsLocalDataSource =
+                new NewsLocalDataSource(getNewsDao(application), sharedPreferencesUtil,
+                        dataEncryptionUtil);
+
+        return new UserRepository(userRemoteAuthenticationDataSource,
+                userDataRemoteDataSource, newsLocalDataSource);
     }
 }
